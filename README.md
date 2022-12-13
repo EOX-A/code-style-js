1fe9605234577f672eb08dd6cd0a90716b5068f8
silvester-pari <silvester.pari@eox.at>
2022-12-13 11:35:31 +0100

    feat!: introduce prettier

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # EOX Vue ESLint config
 ## Installation
 ```js
-npm install --save-dev eslint EOX-A/eslint-config-vue eslint-plugin-vue eslint-plugin-import eslint-plugin-modules-newline
+npm install --save-dev EOX-A/eslint-config-vue
 ```
 ## Usage
 To include this on a project, add to `.eslintrc.js`:
@@ -9,6 +9,10 @@ To include this on a project, add to `.eslintrc.js`:
 module.exports = {
   extends: ['@eox/eslint-config-vue']
 }
+```
+Also, create a `.prettierrc.json` file to let the editor know prettier is used in this project
+```js
+echo {} > .prettierrc.json
 ```
 
 Please check this [link](https://eslint.org/docs/rules/) for the meaning of all the rules.
--- index.js
+++ index.js
@@ -5,32 +5,10 @@ module.exports = {
     browser: true
   },
   extends: [
-    'plugin:vue/strongly-recommended', '@vue/airbnb',
+    "eslint:recommended",
+    "plugin:vue/recommended",
+    "prettier"
   ],
-  plugins: [
-    'modules-newline',
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
   rules: {
-    'global-require': 'off',
-    'import/no-dynamic-require': 'off',
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-underscore-dangle': 'off',
-    'no-prototype-builtins': 'off',
-    'no-plusplus': 'off',
-    'no-return-assign': 'off',
-    'no-mixed-operators': 'off',
-    'no-bitwise': 'off',
-    'array-element-newline': ['error', 'always'],
-    'array-bracket-newline': ['error', 'always'],
-    'vue/array-bracket-newline': ['error', 'always'],
-    'object-property-newline': ['error', { 'allowAllPropertiesOnSameLine': false }],
-    'vue/object-property-newline': 'error',
-    'object-curly-newline': ['error','always'],
-    'modules-newline/import-declaration-newline': 'error',
-    'modules-newline/export-declaration-newline': 'error',
   },
 };
